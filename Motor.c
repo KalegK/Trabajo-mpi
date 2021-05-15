e5f608a9eebb1d1bfaf86cf605c427899898cf23
Carlos Alegria <carlos.alegria@alu.ucm.cl>
2021-05-14 21:52:47 -0400

    Cambios
    lectura de archivo

--- Motor.c
+++ Motor.c
@@ -3,10 +3,15 @@
 
 #include "mpi.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 main (int argc, char **argv) 
 {
-	
+	FILE * archivo=fopen("in.txt","r");
+    if (archivo==NULL){
+        printf("no abrio");
+    }else{
+        printf("se leyo correctamente");
+    }
 }
 
 
