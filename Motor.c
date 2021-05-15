e669a43591e49c5378f3aa75115696d35481ab82
Carlos Alegria <carlos.alegria@alu.ucm.cl>
2021-05-14 22:07:38 -0400

    Update Motor.c

--- Motor.c
+++ Motor.c
@@ -1,17 +1,20 @@
-//Compilacion: mpicxx programa_mpi.c -O3
+//Compilacion: mpicxx Motor.c -O3
 //Ejecucion: mpirun -np 12 ./a.out
 
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 main (int argc, char **argv) 
-{
-	FILE * archivo=fopen("in.txt","r");
-    if (archivo==NULL){
-        printf("no abrio");
-    }else{
-        printf("se leyo correctamente");
-    }
+{   
+    char *palabra;
+
+	FILE * archivo=fopen("in.txt","r");   /* Abre el archivo archivo.txt para lectura de texto */
+        if (archivo==NULL){
+            printf("no abrio");
+            exit(1);
+        }
+ 
 }
 
 
