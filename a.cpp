6e5b8d12a2904253ec3c9f32ffc329918f707c20
Carlos Alegria <carlos.alegria@alu.ucm.cl>
2021-05-18 17:00:46 -0400

    di
    di

--- a.cpp
+++ a.cpp
@@ -1,5 +1,5 @@
 //Compilacion: mpicxx a.cpp -O3
-//Ejecucion: mpirun -np 4 ./a.out
+//Ejecucion: mpirun -np 4 ./a.out <in.txt
 #include "mpi.h"
 #include <stdio.h>
 #include <map>
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
 {
 	int num_documentos, cant_terminos, cant_consultas, ID, nproc;
 	int buffer_nodo[100];
+	int buffer_documentos[100];
 	char titulo[1000];
 	char termino[1000];
 	char consulta[1000];
@@ -34,10 +35,12 @@ int main(int argc, char **argv)
 			getchar();
 			scanf("%s", titulo);
 			cout << "titulo = " << titulo << endl;
+			
 		
 			scanf("%d", &cant_terminos);
 			cout << "cant_terminos = " << cant_terminos << endl;
 			getchar();
+			MPI_Send( &cant_terminos , 3, MPI_INT, 1,  0 , MPI_COMM_WORLD);
 
 			for (int j = 0; j < cant_terminos; ++j)
 			{
@@ -62,9 +65,13 @@ int main(int argc, char **argv)
 		MPI_Recv(buffer_nodo , 1 , MPI_INT ,MPI_ANY_SOURCE , MPI_ANY_TAG, MPI_COMM_WORLD, &var_status);
 		printf("\n\nNODO[%d] :: Dato recibido = %d :: ID Emisor = %d ::\n\n", ID, buffer_nodo[0], var_status.MPI_SOURCE, var_status.MPI_TAG);
 		int arreglo_documentos[buffer_nodo[0]];
-	
+		int length = sizeof(arreglo_documentos)/sizeof(int);
+		cout <<   length<< endl;
 
+		MPI_Recv(buffer_documentos , 3 , MPI_INT ,MPI_ANY_SOURCE , MPI_ANY_TAG, MPI_COMM_WORLD, &var_status);
+		printf(" cantidad de terminos = %d %d %d ::::\n\n", buffer_documentos[0], buffer_documentos[1], buffer_documentos[2],var_status.MPI_SOURCE, var_status.MPI_TAG);
 
+		
 	}
 	
 
